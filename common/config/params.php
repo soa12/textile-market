5b8cbfdfcacfdb54fb28f62d4ad0540e2d344364
Sabyrzhan Aspombitov <sabyrzhan.aspombitov@corp.oft-e.com>
2020-01-27 03:26:44 +0700

    initial

---
+++ common/config/params.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'adminEmail' => 's.aspom@yandex.ru',
+    'supportEmail' => 's.aspom@yandex.ru',
+    'senderEmail' => 's.aspom@yandex.ru',
+    'senderName' => 'Магазин тканей и фурнитуры',
+    'user.passwordResetTokenExpire' => 3600,
+];
